219c7bcfd2cc770974e22ee7234d0719e45330c1
mateuszbabski <babskimateusz@gmail.com>
2023-10-27 12:21:52 +0200

    added functions to logger

--- src/kivi_logger.erl
+++ src/kivi_logger.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %% @author: Mateusz Babski
-%% @last_updated: 26.10.2023
+%% @last_updated: 27.10.2023
 %%
 %% @doc kivi simple key-value database - logger
 %% @end
@@ -8,25 +8,60 @@
 
 -module(kivi_logger).
 
--export([]).
+-export([start/1,
+         stop/1,
+         info/1,
+         info/2,
+         warn/1,
+         warn/2,
+         error/1,
+         error/2]).
 
 %% API
 %% logger prints message on screen and saves it to dedicated file
-info(Msg) ->    
-    %% logs to screen
+%% start logger - create and open file for logs
+start(FilePath) ->
+    {ok, File} = file:open(FilePath, [write, append]),
+    {ok, #state{file=File}}.
+
+%% stop logger - stop and close file for logs
+stop(State) ->
+    file:close(State#state.file).
+
+info(State, Msg) -> 
     CurrentTime = kivi_datetime:get_current_datetime(),
-    io:format("[INFO] ~p: ~p",[CurrentTime], [Msg]).
-    %% saves to file
-    %error_logger:info_msg(Msg).
+    ParsedTime = kivi_parsers:parse_datetime(CurrentTime),    
+    Message = io:format("[INFO][~p]: ~p~n",[ParsedTime, Msg]),
+    {ok, _} = file:write(State#state.file, io_lib:format("~s~n", [Message])),
+    State.
+
+warn(State, Msg) -> 
+    CurrentTime = kivi_datetime:get_current_datetime(),
+    ParsedTime = kivi_parsers:parse_datetime(CurrentTime),
+    Message = io:format("[WARN][~p]: ~p~n",[ParsedTime, Msg]),
+    {ok, _} = file:write(State#state.file, io_lib:format("~s~n", [Message])),
+    State.
+
+error(State, Msg) -> 
+    CurrentTime = kivi_datetime:get_current_datetime(),
+    ParsedTime = kivi_parsers:parse_datetime(CurrentTime),
+    Message = io:format("[ERROR][~p]: ~p~n",[ParsedTime, Msg]),
+    {ok, _} = file:write(State#state.file, io_lib:format("~s~n", [Message])),
+    State.
+
+info(Msg) ->    
+    CurrentTime = kivi_datetime:get_current_datetime(),
+    ParsedTime = kivi_parsers:parse_datetime(CurrentTime),    
+    io:format("[INFO][~p]: ~p~n",[ParsedTime, Msg]).
 
 warn(Msg) ->    
-    %% logs to screen
     CurrentTime = kivi_datetime:get_current_datetime(),
-    io:format("[WARN] ~p: ~p",[CurrentTime], [Msg]).
+    ParsedTime = kivi_parsers:parse_datetime(CurrentTime),
+    io:format("[WARN][~p]: ~p~n",[ParsedTime, Msg]).
 
 error(Msg) ->    
-    %% logs to screen
     CurrentTime = kivi_datetime:get_current_datetime(),
-    io:format("[ERROR] ~p: ~p",[CurrentTime], [Msg]).
+    ParsedTime = kivi_parsers:parse_datetime(CurrentTime),
+    io:format("[ERROR][~p]: ~p~n",[ParsedTime, Msg]).
 
 
