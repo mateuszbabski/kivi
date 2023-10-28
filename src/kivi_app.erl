3560b500f1df6eb574573ea8017ce7caf02ca72f
mateuszbabski <babskimateusz@gmail.com>
2023-10-28 21:33:45 +0200

    corrected application function

--- src/kivi_app.erl
+++ src/kivi_app.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %% @author: Mateusz Babski
-%% @last_updated: 26.10.2023
+%% @last_updated: 28.10.2023
 %%
 %% @doc kivi simple key-value database - main app
 %% @end
@@ -18,8 +18,6 @@ start(_StartType, _StartArgs) ->
     kivi_sup:start_link().
 
 stop(_State) ->
-    LoggerState = application:get_env(kivi, logger_state),
+    {ok, LoggerState} = application:get_env(kivi, logger_state),
     kivi_logger:stop_logger(LoggerState),
     ok.
-
-%% internal functions
