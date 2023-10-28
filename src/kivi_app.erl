6cec0746886de945ec8786c722373666aed0ca95
mateuszbabski <babskimateusz@gmail.com>
2023-10-28 07:56:22 +0200

    corrected app starter

--- src/kivi_app.erl
+++ src/kivi_app.erl
@@ -13,9 +13,13 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    {ok, LoggerState} = kivi_logger:start_logger(),
+    application:set_env(kivi, logger_state, LoggerState),
     kivi_sup:start_link().
 
 stop(_State) ->
+    LoggerState = application:get_env(kivi, logger_state),
+    kivi_logger:stop_logger(LoggerState),
     ok.
 
 %% internal functions
