022c0e3467a9bff9abd159a47ebdc9b480cb851d
mateuszbabski <babskimateusz@gmail.com>
2023-10-29 08:54:14 +0100

    added get timestamp and local time

--- src/utils/kivi_datetime.erl
+++ src/utils/kivi_datetime.erl
@@ -8,11 +8,18 @@
 
 -module(kivi_datetime).
 
--export([get_current_datetime/0]).
+-export([get_current_datetime/0, 
+         get_timestamp/0,
+         get_current_localtime/0
+        ]).
 
 -spec get_current_datetime() -> erlang:datetime().
 get_current_datetime() -> 
     erlang:universaltime().
+
+-spec get_current_localtime() -> erlang:datetime().
+get_current_localtime() -> 
+    erlang:localtime().
 
 -spec get_timestamp() -> erlang:timestamp().
 get_timestamp() ->
