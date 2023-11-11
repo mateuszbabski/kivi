26c71fb1b755b4ab84074e667109e0fb8a7b89d2
mateuszbabski <babskimateusz@gmail.com>
2023-11-11 14:28:53 +0100

    Corrected api functions and loop

--- src/kivi_client.erl
+++ src/kivi_client.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %% @author: Mateusz Babski
-%% @last_updated: 10.11.2023
+%% @last_updated: 11.11.2023
 %%
 %% @doc kivi simple key-value database - client side module
 %% @end
@@ -33,40 +33,52 @@ start_link() ->
 
 loop() ->
     receive
-        {add, ok, NewEntry} -> 
-            kivi_logger:log(warn, "ADD OK"),
+        {add, ok, _NewEntry} ->
+            kivi_logger:log(info, "Key added successfully."),
             loop();
 
-        {add, error} -> 
-            kivi_logger:log(warn, "ADD NOT OK"),
+        {add, error} ->
+            kivi_logger:log(error, "Adding key failed - Key exists in database."),
             loop();
 
-        {update, ok, UpdatedEntry} ->
-            kivi_logger:log(warn, "UPDATE OK"),
+        {update, ok, _UpdatedEntry} ->
+            kivi_logger:log(info, "Key updated successfully."),
             loop();
 
         {update, error} ->
-            kivi_logger:log(warn, "UPDATE NOT OK"),
+            kivi_logger:log(error, "Updating key failed - Key doesn't exist in database."),
             loop();
 
-        {delete, ok, Key} ->
-            kivi_logger:log(warn, "DELETED"),
+        {get, ok, _Key} ->
+            kivi_logger:log(info, "Key returned successfully."),
+            loop();
+
+        {get, error} ->
+            kivi_logger:log(error, "Returning key failed - Key doesn't exist in database."),
+            loop();
+
+        {get_all, ok, _List} ->
+            kivi_logger:log(info, "Keys returned successfully."),
+            loop();
+
+        {delete, ok, _Key} ->
+            kivi_logger:log(info, "Successfully deleted key."),
             loop();
 
         {delete_all, ok} ->
-            kivi_logger:log(warn, "DELETED ALL"),
+            kivi_logger:log(info, "Successfully deleted all keys."),
             loop();
 
-        {sort, ok, SortedData} ->
-            kivi_logger:log(warn, "SORTED"),
+        {sort, ok, _SortedData} ->
+            kivi_logger:log(info, "Successfully returned all sorted keys."),
             loop();
 
-        {get_size, ok, Size} ->
-            kivi_logger:log(warn, "SIZED"),
+        {get_size, ok, _Size} ->
+            kivi_logger:log(info, "Successfully returned number of keys in database."),
             loop();
 
         _ -> 
-            kivi_logger:log(warn, "unhandled response"),
+            kivi_logger:log(warn, "Unhandled response"),
             loop()
     end.
 
@@ -75,57 +87,50 @@ add(Key, Value) ->
     LogMessage = io_lib:format("Trying to add to Database - Key: ~s, Value: ~s", [Key, Value]),
     kivi_logger:log(info, LogMessage),
     gen_server:cast({global, kivi_server}, {add, Key, Value}),
-    io:format("Add operation ok\n"),
-    kivi_logger:log(info, "Add operation ok").
+    io:format("Add operation sent to server.\n").
 
 -spec update(Key :: string(), Value :: string()) -> ok | {badargument, string()}.
 update(Key, Value) ->
     LogMessage = io_lib:format("Trying to update Key: ~s", [Key]),
     kivi_logger:log(info, LogMessage),
     gen_server:cast({global, kivi_server}, {update, Key, Value}),
-    io:format("Update operation ok\n"),
-    kivi_logger:log(info, "Update operation ok").
+    io:format("Update operation sent to server.\n").
 
 -spec get(Key :: string()) -> map() | binary().
 get(Key) ->
     LogMessage = io_lib:format("Trying to get ~s from Database", [Key]),
     kivi_logger:log(info, LogMessage),
     gen_server:call({global, kivi_server}, {get, Key}),
-    io:format("Get operation ok\n"),
-    kivi_logger:log(info, "Get operation ok").
+    io:format("Get operation sent to server.\n").
 
 -spec get_all() -> map().
 get_all() ->
     kivi_logger:log(info, "Trying to get all keys from Database"),
     gen_server:call({global, kivi_server}, {get_all}),
-    io:format("Get_all operation ok\n"),
-    kivi_logger:log(info, "Get_all operation ok").
+    io:format("Get_all operation sent to server.\n").
 
 -spec delete(Key :: string()) -> ok | {badargument, string()}.
 delete(Key) ->
     LogMessage = io_lib:format("Trying to delete key ~s from Database", [Key]),
     kivi_logger:log(info, LogMessage),
     gen_server:cast({global, kivi_server}, {delete, Key}),
-    io:format("Delete operation ok\n"),
-    kivi_logger:log(info, "Delete operation ok").
+    io:format("Delete operation sent to server.\n").
 
 -spec delete_all() -> ok.
 delete_all() ->
     kivi_logger:log(info, "Trying to delete all keys from Database"),
-    gen_server:cast({global, kivi_server}, {delete_all}),
-    kivi_logger:log(info, "Delete_all operation ok"),
-    io:format("Delete_all operation ok\n").
+    gen_server:cast({global, kivi_server}, {delete_all}),    
+    io:format("Delete_all operation sent to server\n").
 
 -spec get_size() -> integer().
 get_size() ->
     kivi_logger:log(info, "Getting number of elements in database"),
     gen_server:call({global, kivi_server}, {get_size}),
-    io:format("Get_size operation ok\n"),
-    kivi_logger:log(info, "Get_size operation ok").
+    io:format("Get_size operation sent to server\n").
 
--spec sort(string()) -> term().
+-spec sort(string()) -> ok.
 sort(SortingBy) ->
-    kivi_logger:log(info, "Trying to sort by ~s and returning list of sorted keys from database", [SortingBy]),
+    LogMessage = io_lib:format("Trying to sort by ~s and returning list of sorted keys from database", [SortingBy]),
+    kivi_logger:log(info, LogMessage),
     gen_server:call({global, kivi_server}, {sort, SortingBy}),
-    io:format("Sort operation ok\n"),
-    kivi_logger:log(info, "Sort operation ok").
+    io:format("Sort operation sent to server.\n").
