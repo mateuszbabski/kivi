12d8b6f34e285695c5fa5b4b8a7d9834e3aba782
mateuszbabski <babskimateusz@gmail.com>
2023-11-25 18:01:55 +0100

    added functions to work with tcp

--- src/kivi_client.erl
+++ src/kivi_client.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %% @author: Mateusz Babski
-%% @last_updated: 24.11.2023
+%% @last_updated: 25.11.2023
 %%
 %% @doc kivi simple key-value database - client side module
 %% @end
@@ -101,70 +101,134 @@ loop() ->
 add(Key, Value) ->
     LogMessage = io_lib:format("Trying to add to Database - Key: ~s, Value: ~s", [Key, Value]),
     kivi_logger:log(info, LogMessage),
-    gen_server:cast({global, kivi_server}, {add, Key, Value}),
+    Request = {add, Key, Value},
+    send_request(Request),
     io:format("Add operation sent to server.\n").
 
 -spec update(Key :: string(), Value :: string()) -> ok | {badargument, string()}.
 update(Key, Value) ->
     LogMessage = io_lib:format("Trying to update Key: ~s", [Key]),
     kivi_logger:log(info, LogMessage),
-    gen_server:cast({global, kivi_server}, {update, Key, Value}),
+    Request = {update, Key, Value},
+    send_request(Request),
     io:format("Update operation sent to server.\n").
 
 -spec get(Key :: string()) -> map() | binary().
 get(Key) ->
     LogMessage = io_lib:format("Trying to get ~s from Database", [Key]),
     kivi_logger:log(info, LogMessage),
-    gen_server:call({global, kivi_server}, {get, Key}),
+    Request = {get, Key},
+    send_request(Request),
     io:format("Get operation sent to server.\n").
 
 -spec get_all() -> map().
 get_all() ->
     kivi_logger:log(info, "Trying to get all keys from Database"),
-    gen_server:call({global, kivi_server}, {get_all}),
+    Request = {get_all},
+    send_request(Request),
     io:format("Get_all operation sent to server.\n").
 
 -spec delete(Key :: string()) -> ok | {badargument, string()}.
 delete(Key) ->
     LogMessage = io_lib:format("Trying to delete key ~s from Database", [Key]),
     kivi_logger:log(info, LogMessage),
-    gen_server:cast({global, kivi_server}, {delete, Key}),
+    Request = {delete, Key},
+    send_request(Request),
     io:format("Delete operation sent to server.\n").
 
 -spec delete_all() -> ok.
 delete_all() ->
     kivi_logger:log(info, "Trying to delete all keys from Database"),
-    gen_server:cast({global, kivi_server}, {delete_all}),    
+    Request = {delete_all},
+    send_request(Request),   
     io:format("Delete_all operation sent to server\n").
 
 -spec get_size() -> integer().
 get_size() ->
     kivi_logger:log(info, "Getting number of elements in database"),
-    gen_server:call({global, kivi_server}, {get_size}),
+    Request = {get_size},
+    send_request(Request),
     io:format("Get_size operation sent to server\n").
 
 -spec sort(string()) -> ok.
 sort(SortingBy) ->
     LogMessage = io_lib:format("Trying to sort by ~s and returning list of sorted keys from database", [SortingBy]),
     kivi_logger:log(info, LogMessage),
-    gen_server:call({global, kivi_server}, {sort, SortingBy}),
+    Request = {sort, SortingBy},
+    send_request(Request),
     io:format("Sort operation sent to server.\n").
 
 %%%% send_request to tcp
-%send_request(Request) ->
-%    {ok, Socket} = gen_tcp:connect({127, 0, 0, 1}, 8081, []),
-%    gen_tcp:send(Socket, encode_request(Request)),
-%    receive
-%        {tcp, Socket, Bin} ->
-%            Response = decode_response(Bin),
-%            handle_response(Response),
-%            gen_tcp:close(Socket);
-%        _ ->
-%            io:format("Error receiving response from server"),
-%            gen_tcp:close(Socket)
-%    end.
+send_request(Request) ->
+    {ok, Socket} = gen_tcp:connect({127, 0, 0, 1}, 8081, []),
+    gen_tcp:send(Socket, kivi_parsers:encode_message(Request)),
+    receive
+        {tcp, Socket, Bin} ->
+            LogMessage = io_lib:format("Response ~p", [Bin]),
+            kivi_logger:log(info, LogMessage),
+            handle_response(Bin),
+            gen_tcp:close(Socket);
 
-% handle_response(Response) ->
-%   receive
-%   add cases of response
-%   end.
+        _ ->
+            io:format("Error receiving response from server"),
+            gen_tcp:close(Socket)
+    end.
+
+%%%% handle_response from tcp
+handle_response(Response) ->
+   DecodedResponse = kivi_parsers:decode_response(Response),
+   case DecodedResponse of
+        {add, ok, Key, NewEntry} ->
+            kivi_logger:log(info, "Key added successfully."),
+            kivi_printer:print_entry(Key, NewEntry);
+
+        {add, error} ->
+            Message = "Adding key failed - Key exists in database.",
+            kivi_logger:log(error, Message),
+            kivi_printer:print_message(Message);
+
+        {update, ok, Key, UpdatedEntry} ->
+            kivi_logger:log(info, "Key updated successfully."),
+            kivi_printer:print_entry(Key, UpdatedEntry);
+
+        {update, error} ->
+            Message = "Updating entry failed - Key doesn't exist in database.",
+            kivi_logger:log(error, Message),
+            kivi_printer:print_message(Message);
+
+        {get, ok, Key, Entry} ->
+            kivi_logger:log(info, "Key returned successfully."),
+            kivi_printer:print_entry(Key, Entry);
+
+        {get, error} ->
+            Message = "Returning entry failed - Key doesn't exist in database.",
+            kivi_logger:log(error, Message),
+            kivi_printer:print_message(Message);
+
+        {get_all, ok, List} ->
+            kivi_logger:log(info, "List returned successfully."),
+            kivi_printer:print(List);
+
+        {delete, ok, _Key} ->
+            Message = "Successfully deleted key.",
+            kivi_logger:log(info, Message),
+            kivi_printer:print_message(Message);
+
+        {delete_all, ok} ->
+            Message = "Successfully deleted all keys.",
+            kivi_logger:log(info, Message),
+            kivi_printer:print_message(Message);
+        
+        {sort, ok, SortedData} ->
+            kivi_logger:log(info, "Successfully returned all sorted keys."),
+            kivi_printer:print(SortedData);
+
+        {get_size, ok, Size} ->
+            kivi_logger:log(info, "Successfully returned number of keys in database."),
+            kivi_printer:print_size(Size);
+
+        _ ->
+            Message = "Unhandled response",
+            kivi_logger:log(warn, Message),
+            kivi_printer:print_message(Message)
+   end.
